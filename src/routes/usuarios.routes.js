5dfc029fbc3b5347882d7be02ea9a473ef333c77
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2024-10-24 16:55:13 -0300

    feat: create get all users

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,4 +4,13 @@ const usuariosRoutes = Router();
 
 let usuarios = [];
 
+
+usuariosRoutes.get("/", (req, res) => {    
+    return res.status(200).json({
+        message: 
+        usuarios.length == 0 ? "Nenhum usuário encontrado" : 
+        `Total de usuários: ${usuarios.length}`, usuarios,
+    });
+});
+
 export default usuariosRoutes;
